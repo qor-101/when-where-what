f5806a6ec9f7b03417038ceab8b9adbe29fe766a
qor-101 <qor6119@gmail.com>
2021-11-17 14:54:37 +0530

    Added Login-Signup

--- WhenWhereWhat/web/urls.py
+++ WhenWhereWhat/web/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-] 
+    path('',views.index,name='homepage'),
+    path('login',views.login_user,name='login_user'),
+    path('signup',views.signup_user,name='signup_user'),
+]
--- WhenWhereWhat/web/views.py
+++ WhenWhereWhat/web/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, request
+from django.template import loader
+from django import forms
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from datetime import date
+import sqlite3
 
 # Create your views here.
+
+@csrf_exempt
+def index(request):
+    return render(request,"homepage.html")
+
+@csrf_exempt
+def login_user(request):
+    cand_usn = request.POST['usn']
+    cand_pwd = request.POST['pwd']
+    user = authenticate(request, username = cand_usn, password = cand_pwd)
+    if user is not None:
+        login(request, user)
+        return redirect('/profile')
+    else: 
+        # create alert as login failed
+        return redirect('/')
+
+@csrf_exempt
+def signup_user(request):
+    new_usn = request.POST['usn']
+    new_pwd = request.POST['pwd']
+    new_email = request.POST['mail']
+    new_fname = request.POST['fname']
+    new_lname = request.POST['lname']
+    
+    usn_list = get_user()
+    if(new_usn in usn_list):
+        #create alert usn npt available
+        return redirect("/")
+    else:
+        user = User.objects.create_user(new_usn, email=new_email, password=new_pwd)
+        user.first_name = new_fname
+        user.last_name = new_lname
+        user.save()
+        
+        if user is not None:
+            login(request,user)
+            return redirect('/profile')
+        else:
+            return redirect('/') 
