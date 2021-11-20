0094671661e1f45a61ece8916f9831e3a76ecd66
qor-101 <qor6119@gmail.com>
2021-11-20 14:05:42 +0530

    Added User Login and Preferences

--- WhenWhereWhat/web/views.py
+++ WhenWhereWhat/web/views.py
@@ -18,11 +18,6 @@ from urllib.parse import urlparse
 
 # Create your views here.
 
-
-
-
-
-
 #API_KEY = "e6a2a3f2f2ad4258adca6d6e017584d2"
 
 def top_news(request):
@@ -81,10 +76,20 @@ def search_news(request):
     }
     return render(request , 'index.html' , context)
 
+def get_user():
+    mydb = sqlite3.connect("db.sqlite3")
+    mycursor = mydb.cursor()  
+    res = mycursor.execute('SELECT username FROM auth_user')
+    lis=[]
+    for i in res:
+        lis.append(i[0])
+    mydb.close()
+    return lis
+
 @csrf_exempt
 def login_user(request):
-    cand_usn = request.POST['usn']
-    cand_pwd = request.POST['pwd']
+    cand_usn = request.POST['UN']
+    cand_pwd = request.POST['PW']
     user = authenticate(request, username = cand_usn, password = cand_pwd)
     if user is not None:
         login(request, user)
@@ -95,21 +100,43 @@ def login_user(request):
 
 @csrf_exempt
 def signup_user(request):
+    
+    new_fname = request.POST['fname']
+    new_email = request.POST['mail']
     new_usn = request.POST['usn']
     new_pwd = request.POST['pwd']
-    new_email = request.POST['mail']
-    new_fname = request.POST['fname']
-    new_lname = request.POST['lname']
+    pref = []
+    cat1 = request.POST.get('cat1',False)
+    cat2 = request.POST.get('cat2',False)
+    cat3 = request.POST.get('cat3',False)
+    cat4 = request.POST.get('cat4',False)
+    cat5 = request.POST.get('cat5',False)
+    if cat1:
+        pref.append(cat1)
+    if cat2:
+        pref.append(cat2)
+    if cat3:
+        pref.append(cat3)
+    if cat4:
+        pref.append(cat4)
+    if cat5:
+        pref.append(cat5)
     
     usn_list = get_user()
     if(new_usn in usn_list):
-        #create alert usn npt available
+        #create alert usn not available
         return redirect("/")
     else:
         user = User.objects.create_user(new_usn, email=new_email, password=new_pwd)
         user.first_name = new_fname
-        user.last_name = new_lname
         user.save()
+        # saving preferences in preference table
+        prefs = ",".join(pref)
+        mydb = sqlite3.connect("db.sqlite3")
+        mycursor = mydb.cursor()  
+        mydb.execute("INSERT INTO preference VALUES(?,?)",(new_usn,prefs))
+        mydb.commit()
+        mydb.close()
         
         if user is not None:
             login(request,user)
