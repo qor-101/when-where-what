822b85cd0efca75a7b6eac4ca083392d5f7caac9
qor-101 <qor6119@gmail.com>
2021-12-28 15:09:49 +0530

    Added UN ASIA RSS

--- WhenWhereWhat/rss/urls.py
+++ WhenWhereWhat/rss/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('toi/', toi, name='toi'),
     path('indiatoday/', indiatoday, name='indiatoday'),
-    path('news18/',news18,name='news18')
+    path('news18/',news18,name='news18'),
+    path('unasia/',unasia,name='unasia')
 ]
--- WhenWhereWhat/rss/views.py
+++ WhenWhereWhat/rss/views.py
@@ -17,25 +17,26 @@ def toi(request):
     
     articles = []
     dx = fp.parse('https://timesofindia.indiatimes.com/rssfeedstopstories.cms')
-    n = min(len(dx.entries),33)
+    n = min(len(dx.entries),28)
     for i in range(n):
         a={
             'title':dx.entries[i].title,
             'url':dx.entries[i].link,
-            'published':dx.entries[i].published,
+            'publishedAt':dx.entries[i].published,
+            'description':dx.entries[i].summary,
         }
         articles.append(a)
     context = {
         'zero' : articles[0],
         'news' : articles[1:],
     }
-    return render(request , 'index2.html' , context)
+    return render(request , 'toi.html' , context)
 
 def indiatoday(request):
     
     dx = fp.parse('https://www.indiatoday.in/rss/1206584')
     articles = []
-    n = min(len(dx.entries),33)
+    n = min(len(dx.entries),28)
     for i in range(n):
         m = dx.entries[i].summary.find("src")
         x = dx.entries[i].summary[m:].split(" ")
@@ -59,7 +60,7 @@ def news18(request):
     
     dx = fp.parse('https://www.news18.com/rss/india.xml')
     articles = []
-    n = min(len(dx.entries),33)
+    n = min(len(dx.entries),28)
     for i in range(n):
         a={
             'title':dx.entries[i].title,
@@ -67,6 +68,28 @@ def news18(request):
             'publishedAt':dx.entries[i].published,
             'urlToImage':dx.entries[i].media_content[0]['url'],
             'description':dx.entries[i].description[:100],
+        }
+        articles.append(a)
+    
+    context = {
+        'zero' : articles[0],
+        'news' : articles[1:],
+    }
+    
+    return render(request , 'index.html' , context)
+
+def unasia(request):
+    
+    dx = fp.parse('https://news.un.org/feed/subscribe/en/news/region/asia-pacific/feed/rss.xml')
+    articles = []
+    n = min(len(dx.entries),28)
+    for i in range(n):
+        a={
+            'title':dx.entries[i].title,
+            'url':dx.entries[i].link,
+            'publishedAt':dx.entries[i].published,
+            'urlToImage':dx.entries[i].links[1].href,
+            'description':dx.entries[i].summary,
         }
         articles.append(a)
     
